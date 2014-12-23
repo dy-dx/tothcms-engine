f7ec7fc3933e0fb252f0dac4838d58a0a4f34e7d
Chris K <christopherjkim@gmail.com>
2014-12-22 22:56:21 -0500

    initial rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TothcmsEngine::Application.config.secret_token = '81747f27ee8d7eb2e7853fdfc5b9a54ca052969489b698bc0ab2400079d0908a8cba21a5d46a3e50d1d39f4890b481da6d562c826b28c28a215f987d56a8c80f'
